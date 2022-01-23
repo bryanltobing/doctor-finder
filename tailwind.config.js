14f22fc6f88a8eb94d362db21199b7f719f20f66
bryantobing12 <bryantobing0@gmail.com>
2022-01-23 22:38:33 +0700

    Configure font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    fontFamily: {
+      ...defaultTheme.fontFamily,
+      sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [],
